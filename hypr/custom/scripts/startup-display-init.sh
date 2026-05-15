032c79a57b7e7a78c5e6727ffcb5cc4fe6d24dcb
Digit <connorsommervilles@gmail.com>
2026-05-15 11:25:20 +0200

    Speeding up the script

--- hypr/custom/scripts/startup-display-init.sh
+++ hypr/custom/scripts/startup-display-init.sh
@@ -4,7 +4,7 @@ MONITOR="DP-6"
 CURSOR_X=3200
 CURSOR_Y=720
 
-sleep 1
+sleep 0.2
 
 hyprctl dispatch "hl.dsp.focus({ monitor = \"${MONITOR}\" })"
 hyprctl dispatch "hl.dsp.cursor.move({ x = ${CURSOR_X}, y = ${CURSOR_Y} })"
